40126566be53867be80a812fcf99a31f6b9fbf79
Lucca <luccajbastos@gmail.com>
2021-10-18 12:27:16 -0300

    1.0 completo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as PageController from '../controllers/pageController';
 import * as SearchController from '../controllers/searchController';
-
+ 
 const router = Router();
 
 router.get('/', PageController.home);
